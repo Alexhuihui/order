181177108ec6ece20846e937cd668382a8cafb8a
wangyonghui <2930807240@qq.com>
2019-03-01 21:49:04 +0800

    整合借口

--- src/main/java/com/imooc/order/client/ProductClient.java
+++ src/main/java/com/imooc/order/client/ProductClient.java
@@ -1,7 +1,13 @@
 package com.imooc.order.client;
 
+import com.imooc.order.domain.ProductInfo;
+import com.imooc.order.dto.CartDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 /**
  * @author 汪永晖
@@ -11,4 +17,10 @@ public interface ProductClient {
 
     @GetMapping("/msg")
     String productMsg();
+
+    @PostMapping("/product/listForOrder")
+    List<ProductInfo> listForOrder(@RequestBody List<String> productIdList);
+
+    @PostMapping("/product/decreaseStock")
+    void decreaseStock(@RequestBody List<CartDTO> cartDTOList);
 }
--- src/main/java/com/imooc/order/controller/ClientController.java
+++ src/main/java/com/imooc/order/controller/ClientController.java
@@ -1,10 +1,16 @@
 package com.imooc.order.controller;
 
 import com.imooc.order.client.ProductClient;
+import com.imooc.order.domain.ProductInfo;
+import com.imooc.order.dto.CartDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author 汪永晖
@@ -21,5 +27,18 @@ public class ClientController {
         String response = productClient.productMsg();
         log.info("response={}", response);
         return response;
+    }
+
+    @GetMapping("/getProductList")
+    public String getProductList() {
+        List<ProductInfo> productInfoList = productClient.listForOrder(Arrays.asList("2"));
+        log.info("response={}", productInfoList);
+        return "ok";
+    }
+
+    @GetMapping("/productDecreaseStock")
+    public String productDecreaseStock() {
+        productClient.decreaseStock(Arrays.asList(new CartDTO("3", 3)));
+        return "ok";
     }
 }
