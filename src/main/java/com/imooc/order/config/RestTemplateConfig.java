c93838d2619f51850ee0a19f2b227c8e912cfe75
wangyonghui <2930807240@qq.com>
2019-03-01 18:17:58 +0800

    测试应用通信

---
+++ src/main/java/com/imooc/order/config/RestTemplateConfig.java
@@ -0,0 +1,19 @@
+package com.imooc.order.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author 汪永晖
+ */
+@Component
+public class RestTemplateConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
